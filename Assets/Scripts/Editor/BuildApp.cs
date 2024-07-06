4357041817038c65df22fd721ee09f9fb9642b57
yuni <RimsEther358@outlook.jp>
2024-07-07 00:53:52 +0900

    build testing

--- Assets/Scripts/Editor/BuildApp.cs
+++ Assets/Scripts/Editor/BuildApp.cs
@@ -5,10 +5,10 @@ public static class BuildApp
     [MenuItem("Build/BuildApp")]
     public static void Build()
     {
-        //windows64のプラットフォームでアプリをビルドする
+        // Windows 64-bit platform build
         BuildPipeline.BuildPlayer(
             new string[] { "Assets/Scenes/SampleScene.unity" },
-            "D:\UnityPackages\Unity_CI_CD_Sampe/SampleApp.exe",
+            "D:\\UnityPackages\\Unity_CI_CD_Sampe\\SampleApp.exe",
             BuildTarget.StandaloneWindows64,
             BuildOptions.None
         );
